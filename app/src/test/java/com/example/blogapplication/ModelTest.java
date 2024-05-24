aa52dfd6b3af595a4a19f6393f92319d59f2bdb3
Arefin Nahid <nahid7ar@gmail.com>
2024-05-24 23:00:34 +0600

    Unit Test

--- app/src/test/java/com/example/blogapplication/ModelTest.java
+++ app/src/test/java/com/example/blogapplication/ModelTest.java
@@ -6,43 +6,108 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class ModelTest {
+
     private Model model;
 
     @Before
     public void setUp() {
-        model = new Model("Title", "Description", "Author", "Date", "ImageURL", "5", "1", "timestamp");
+        model = new Model(
+                "Sample Title",
+                "Sample Description",
+                "Author Name",
+                "2024-05-23",
+                "http://example.com/image.jpg",
+                "10",
+                "1",
+                "1629456789000"
+        );
     }
 
     @Test
-    public void testGetters() {
-        assertEquals("Title", model.getTittle());
-        assertEquals("Description", model.getDesc());
-        assertEquals("Author", model.getAuthor());
-        assertEquals("Date", model.getDate());
-        assertEquals("ImageURL", model.getImg());
-        assertEquals("5", model.getShare_count());
-        assertEquals("1", model.getId());
-        assertEquals("timestamp", model.getTimestamp());
+    public void testGetTittle() {
+        assertEquals("Sample Title", model.getTittle());
     }
 
     @Test
-    public void testSetters() {
+    public void testSetTittle() {
         model.setTittle("New Title");
-        model.setDesc("New Description");
-        model.setAuthor("New Author");
-        model.setDate("New Date");
-        model.setImg("New ImageURL");
-        model.setShare_count("10");
-        model.setId("2");
-        model.setTimestamp("new_timestamp");
-
         assertEquals("New Title", model.getTittle());
+    }
+
+    @Test
+    public void testGetDesc() {
+        assertEquals("Sample Description", model.getDesc());
+    }
+
+    @Test
+    public void testSetDesc() {
+        model.setDesc("New Description");
         assertEquals("New Description", model.getDesc());
+    }
+
+    @Test
+    public void testGetAuthor() {
+        assertEquals("Author Name", model.getAuthor());
+    }
+
+    @Test
+    public void testSetAuthor() {
+        model.setAuthor("New Author");
         assertEquals("New Author", model.getAuthor());
-        assertEquals("New Date", model.getDate());
-        assertEquals("New ImageURL", model.getImg());
+    }
+
+    @Test
+    public void testGetDate() {
+        assertEquals("2024-05-23", model.getDate());
+    }
+
+    @Test
+    public void testSetDate() {
+        model.setDate("2024-06-01");
+        assertEquals("2024-06-01", model.getDate());
+    }
+
+    @Test
+    public void testGetImg() {
+        assertEquals("http://example.com/image.jpg", model.getImg());
+    }
+
+    @Test
+    public void testSetImg() {
+        model.setImg("http://example.com/new_image.jpg");
+        assertEquals("http://example.com/new_image.jpg", model.getImg());
+    }
+
+    @Test
+    public void testGetShareCount() {
         assertEquals("10", model.getShare_count());
+    }
+
+    @Test
+    public void testSetShareCount() {
+        model.setShare_count("20");
+        assertEquals("20", model.getShare_count());
+    }
+
+    @Test
+    public void testGetId() {
+        assertEquals("1", model.getId());
+    }
+
+    @Test
+    public void testSetId() {
+        model.setId("2");
         assertEquals("2", model.getId());
-        assertEquals("new_timestamp", model.getTimestamp());
+    }
+
+    @Test
+    public void testGetTimestamp() {
+        assertEquals("1629456789000", model.getTimestamp());
+    }
+
+    @Test
+    public void testSetTimestamp() {
+        model.setTimestamp("1629456790000");
+        assertEquals("1629456790000", model.getTimestamp());
     }
 }
